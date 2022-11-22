6b61c5d30429edacdc40fa4a87a1f2c3b00c326b
Piotr Chrzanowski <p.chrzanowski@hydro-partner.pl>
2022-11-22 14:37:16 +0100

    v0.4

--- app/controllers/strona_blender_controller.rb
+++ app/controllers/strona_blender_controller.rb
@@ -1,7 +1,13 @@
 class StronaBlenderController < ApplicationController
+    before_action :check_user
 
     def index 
         
+    end
+
+    def check_user
+        @blender = current_user
+        redirect_to new_user_session_path, notice: "Brak dostępu" if @blender.nil?
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,16 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "home#index"
    get 'strona_blender/index'
+
+   devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root #jak jest zalogowany
+    end
+  
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root #jak nie jest zalogowany
+    end
+  end
+
 end
